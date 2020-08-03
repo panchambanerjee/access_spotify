1ec400948b6ba3cf27d78d60cad9a0d4a8c52caf
delayedkarma <panchajanya.banerjee@gmail.com>
2020-08-02 21:02:40 -0700

    Changed module in album art script

--- access_spotify/get_all_album_art.py
+++ access_spotify/get_all_album_art.py
@@ -7,11 +7,11 @@ import urllib.request
 from PIL import Image
 
 
-def get_all_album_art(album_names, album_img_url_dict, album_cover_path):
-    """Downloads and saves highest resolution album artwork for all albums
+def get_album_art(album_name, album_img_url_dict, album_cover_path):
+    """Downloads and saves highest resolution album artwork for the specified album
 
-    :param album_names: List of the album names
-    :type album_names: list
+    :param album_name: Album name
+    :type album_name: str
     :param album_img_url_dict: Dictionary mapping album names to album cover urls
     :type album_img_url_dict: dict
     :param album_cover_path: Path to save album images to
@@ -20,12 +20,11 @@ def get_all_album_art(album_names, album_img_url_dict, album_cover_path):
 
     """
 
-    for album_name in tqdm(album_names):
-        print(f"Downloading album cover for {album_name}")
+    print(f"Downloading album cover for {album_name}")
 
-        # Get album cover
-        url = album_img_url_dict[album_name]
-        image = Image.open(urllib.request.urlopen(url))
+    # Get album cover
+    url = album_img_url_dict[album_name]
+    image = Image.open(urllib.request.urlopen(url))
 
-        image.save(f"{album_cover_path}/{album_name}_art.png")
+    image.save(f"{album_cover_path}/{album_name}_art.png")
 
