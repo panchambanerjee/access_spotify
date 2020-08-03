edf5818d52a4dd2c689d2111e3572dc8b4224321
delayedkarma <panchajanya.banerjee@gmail.com>
2020-08-03 06:29:27 -0700

    Changed package version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 from setuptools import setup
 
 setup(name='access-spotify',
-      version='1.13',
+      version='1.14',
       author='pancham_banerjee',
       packages=setuptools.find_packages(),
       scripts=['./bin/access_script.py'],
