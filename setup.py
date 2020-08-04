eec37875b22a6b1e932763728e29fda70182b7bd
delayedkarma <panchajanya.banerjee@gmail.com>
2020-08-03 19:16:40 -0700

    Added description and email to setup file

--- setup.py
+++ setup.py
@@ -3,10 +3,17 @@
 import setuptools
 from setuptools import setup
 
+with open("requirements.txt", "r") as f:
+    requirements = [line.rstrip("\n") for line in f if line != "\n"]
+
 setup(name='access-spotify',
-      version='1.17',
+      version='1.18',
       author='pancham_banerjee',
+      author_email='panchajanya.banerjee@gmail.com',
       packages=setuptools.find_packages(),
       scripts=['./bin/access_script.py'],
+      install_requires=requirements,
+      license='MIT',
+      description='A package to get all album and track info for an artist by querying the Spotify API'
       )
 
